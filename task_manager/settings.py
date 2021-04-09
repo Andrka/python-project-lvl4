26b06a54510b2076688ce8949e5e2306ec6fd89a
Andrka <lozhnikov.andrei@gmail.com>
2021-04-09 08:48:06 +0300

    Add tasks app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG_KEY')
+DEBUG = os.environ.get('DEBUG_FLAG')
 
-ALLOWED_HOSTS = ['task-----manager.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
